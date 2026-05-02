d4f2a4f77147f3f3efc51002e1ba58bfeff620b9
Ilhame el mabrouki <i.elmabrouki6684@uca.ac.ma>
2026-05-02 12:14:52 +0200

    إضافة صفحة تركيا وتعديل باقي ملفات المطعم

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function showFood(name, price, ing){
-  alert(name + "\nPrix: " + price + " DH\nIngrédients: " + ing);
+function showPrice(name, price) {
+    alert("الأكلة: " + name + "\nالثمن: " + price);
+    // يمكن لينا نطوروه مستقبلاً باش يبان فـ نافذة أجمل (Modal) عوض alert
 }
